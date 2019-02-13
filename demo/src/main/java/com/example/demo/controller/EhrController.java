dc06572704b646bca0ddfa767c2c6a18feebd549
MultipleCrashes <inimitableharish@gmail.com>
2019-02-13 17:41:27 +0530

    DB connector added for mongodb

--- demo/src/main/java/com/example/demo/controller/EhrController.java
+++ demo/src/main/java/com/example/demo/controller/EhrController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/ehr")
 public class EhrController{
 	@ResponseBody
-	@RequestMapping(method = GET, produces = "application/json")
-	public String ehr() {
+	@RequestMapping(method = POST, produces = "application/json")
+	public String ehr(HttpRequest request) {
+		System.out.println("Request object for the EHR API is "+ request);
+		
 		System.out.println("Recording EHR in the system");
 		return "{\"respMgs\" : \"EHR successfully recorded\"}";
+		
 	}
 }
---
+++ demo/src/main/java/com/example/demo/db/connector.java
@@ -0,0 +1,69 @@
+package com.example.demo.db;
+
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClient;
+import com.mongodb.*;
+import com.mongodb.MongoCredential;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.*;
+import java.lang.*;
+import com.mongodb.Cursor;
+
+import java.awt.*;
+
+
+
+class Connector{
+
+public static void main(String args[])
+{
+	
+	try {
+// create mongo client
+		MongoClient mongo = new MongoClient("localhost",27017);
+	// creating credentials 
+		
+		System.out.println("Connected to the datase successfully");
+		// Accessing the database 
+		Connector obj = new Connector();
+		obj.bulkInsertion(mongo);
+		String dbName;
+		String collectionName;
+		obj.iterateCollection(mongo, dbName="ndb", collectionName="collection1");
+		//obj.dbOperations(mongo);
+		//obj.insertRow(mongo);  // passing Db handler 
+	}
+	catch(Exception e) {
+		System.out.println("exception found while connecting to client");
+	}
+}
+
+
+public void bulkInsertion(MongoClient mongo) {
+	DB dbh = mongo.getDB("ndb");
+	DBCollection dbc = dbh.getCollection("collection1");
+	for(int x=1; x<100000; x++) {		
+		BasicDBObject document = new BasicDBObject();
+		document.put("fname", x);
+		dbc.insert(document);
+		System.out.println("Inserting element in db : " +x );
+	}
+	
+}
+
+public void iterateCollection(MongoClient mongo, String dbName, String collectionName)
+{
+	DB dbh = mongo.getDB(dbName);
+	DBCollection dbc = dbh.getCollection(collectionName);
+	dbc.find();
+	DBCursor dbcur = dbc.find();
+	
+	while(dbcur.hasNext()) {
+		System.out.println("Fetched value from db" + dbcur.next());
+	}
+}
+}
